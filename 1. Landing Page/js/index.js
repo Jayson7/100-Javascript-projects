fa18aea7896e593526843e5b5e4e95ac7db72bd5
jayson7 <lexxiijoo70@gmail.com>
2022-12-25 23:04:26 +0100

    landing page

--- 1. Landing Page/js/index.js
+++ 1. Landing Page/js/index.js
@@ -1,5 +1,6 @@
 const toggle_bar = document.querySelector(".fa");
-
+const menu = document.querySelector(".navbar div");
 toggle_bar.addEventListener("click", () => {
-    alert("clicked");
+    menu.classList.toggle("active");
+    menu.classList.toggle("in-active");
 });

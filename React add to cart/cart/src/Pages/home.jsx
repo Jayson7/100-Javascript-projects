75f391fb3934c8d0bfe6cf6fd73271de8f6d18b0
jayson7 <lexxiijoo70@gmail.com>
2022-12-30 04:09:20 +0100

    react cart project

--- React add to cart/cart/src/Pages/home.jsx
+++ React add to cart/cart/src/Pages/home.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Homepage() {
   return (
-    <div>
-      <h1> Welcome to CarMasters </h1>
-      <p>Homepage</p>
+    <div className="container mt-5">
+      <div className="jumbotron">
+        <h1 className="display-4">Hello, world!</h1>
+        <p className="lead">
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+        <hr className="my-4" />
+        <p>
+          It uses utility classes for typography and spacing to space content
+          out within the larger container.
+        </p>
+        <Link className="btn btn-primary btn-lg" to="#" role="button">
+          Learn more
+        </Link>
+      </div>
     </div>
   );
 }

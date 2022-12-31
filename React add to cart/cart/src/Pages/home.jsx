70e1647f40de6021ac305bef199638c7ab1ca891
jayson7 <lexxiijoo70@gmail.com>
2022-12-31 01:41:10 +0100

    react cart project

--- React add to cart/cart/src/Pages/home.jsx
+++ React add to cart/cart/src/Pages/home.jsx
@@ -6,16 +6,21 @@ function Homepage() {
       <div className="jumbotron text-white">
         <h1 className="display-4">We sell What You Desire. SPEED!</h1>
         <p className="lead">
-          This is a simple hero unit, a simple jumbotron-style component for
-          calling extra attention to featured content or information.
+          We offer a trusted, reliable and cost-effective online marketplace for
+          the widest range of quality vehicles around the world. Make 97%
+          remaining payment on vehicle delivery at your doorstep.
         </p>
         <hr className="my-4" />
         <p>
-          It uses utility classes for typography and spacing to space content
-          out within the larger container.
+          End to End Assistance from Listing till Selling with a Dedicated
+          Mindset.
         </p>
-        <Link className="btn btn-primary btn-lg" to="#" role="button">
+        <Link className="btn  btn-info btn-lg" to="#" role="button">
           Learn more
+        </Link>
+
+        <Link className="btn mx-5 btn-primary btn-lg" to="#" role="button">
+          Order now
         </Link>
       </div>
     </div>

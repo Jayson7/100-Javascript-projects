c826cf743cd8d32becacc17df6cb067fba736f04
jayson7 <lexxiijoo70@gmail.com>
2023-01-02 15:02:02 +0100

    react counter

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,11 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <h1>Counter</h1>
 
+      <div className="count">{count}</div>
       <div className="buttons">
         <button>Decrement</button>
         <button>Increment</button>

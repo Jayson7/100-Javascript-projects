3722fee5d751fededecbccc89f2cd83db90b45ad
jayson7 <lexxiijoo70@gmail.com>
2023-01-02 15:15:56 +0100

    react counter

--- counter/src/App.js
+++ counter/src/App.js
@@ -10,7 +10,17 @@ function App() {
 
       <div className="count">{count}</div>
       <div className="buttons">
-        <button onClick={() => setCount(count - 1)}>Decrement</button>
+        <button
+          onClick={() => {
+            if (count === 0) {
+              setCount(0);
+            } else {
+              setCount(count - 1);
+            }
+          }}
+        >
+          Decrement
+        </button>
         <button onClick={() => setCount(count + 1)}>Increment</button>
         <button onClick={() => setCount(0)}>Erase</button>
       </div>

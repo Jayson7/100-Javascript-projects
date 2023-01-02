844b54cdc0427dc4a534b68998fd2a8c4334f253
jayson7 <lexxiijoo70@gmail.com>
2023-01-02 13:57:58 +0100

    javascript counter

--- counter (JavaScript)/index.js
+++ counter (JavaScript)/index.js
@@ -1,5 +1,22 @@
-const value = document.querySelector(".count");
+var values = 0;
 
-const Increase = () => {
-    alert("increase");
-};
+document.querySelector(".count").innerText = values;
+
+function Increase() {
+    values = values + 1;
+    document.querySelector(".count").innerText = values;
+}
+
+function Decrease() {
+    if (values === 0) {
+        pass;
+    } else {
+        values = values - 1;
+        document.querySelector(".count").innerText = values;
+    }
+}
+
+function erase() {
+    values = 0;
+    document.querySelector(".count").innerText = values;
+}
